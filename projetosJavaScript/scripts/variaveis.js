87550ba81318f8a6fd61646211316ca44d91abc1
Henrique Capitulino <georgehenriquecampos@gmail.com>
2024-09-08 22:06:51 -0300

    Variaveis em JavaScript

---
+++ projetosJavaScript/scripts/variaveis.js
@@ -0,0 +1,41 @@
+// Em JavaScript as variaveis não precisam de tipagem exemplo como.
+var texto = "Essa é uma variável do tipo texto sem ser declarado como string ou char como em outras linguagens como Java ou C.";
+console.log(texto);
+
+// Variaveis do numeral e suas operações.
+var n1 = 2; // Exemplo de varial do tipo inteiro sem ser declarado como inteiro.
+var n2 = 2;
+
+var result = n1 + n2; // Exemplo de operação de adição com numeros inteiros.
+console.log("Esse é o resultado de uma operação de adição em js com números inteiros, " + result);
+
+/*
+ * Aqui estão alguns exemplos de operadores aritméticos.
+ * + - Adição
+ * - - Subtração
+ * / - Divisão
+ * * - Multiplicação
+ */
+
+var s1, s2, s3, s4, s5, s6, s7, s8;
+var soma1, soma2, soma3, soma4;
+
+s1 = 3;
+s2 = 2;
+
+s3 = 5;
+s4 = 11;
+
+s5 = 22
+s6 = 5;
+
+s7 = 10;
+s8 = 5;
+
+soma1 = s1 + s2;
+soma2 = s3 - s4;
+
+soma3 = s5 / s6;
+soma4 = s7 * s8;
+
+console.log(`Adição: ${soma1},\nSubtração: ${soma2},\nDivisão: ${soma3},\nMultiplicação: ${soma4}.`);
