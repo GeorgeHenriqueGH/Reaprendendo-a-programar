f109038286db3a8fc8ac0f7c380ef42ea8ddc41b
Henrique Capitulino <georgehenriquecampos@gmail.com>
2024-09-12 01:54:22 -0300

    Operadores de atribuicao.

---
+++ projetosJavaScript/scripts/operadores_de_atribuicao.js
@@ -0,0 +1,33 @@
+var x = 1;
+var y = 2;
+
+// soma
+console.log(x = x + y);
+console.log(x += y);
+
+// subtração
+console.log(x -= y);
+
+// Multiplicação
+console.log(x *= y);
+
+// Divisão
+console.log(x /= y);
+
+// loops
+console.log(x++);
+console.log(x--);
+
+while (x <= 100) {
+    console.log(x);
+    x *= 2;
+}
+
+console.log(x);
+
+while (x >= 0) {
+    console.log(x);
+    x -= 5;
+}
+
+console.log(x);
