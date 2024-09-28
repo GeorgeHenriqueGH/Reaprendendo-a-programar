5f925d8a931296fc394a46352335db4c87d61ff6
Henrique Capitulino <georgehenriquecampos@gmail.com>
2024-09-28 20:19:25 -0300

    script que altera os valores das variavel do css.

---
+++ projetosHTMLeCSS/myblog/js/toogleTheme.js
@@ -0,0 +1,23 @@
+const toggle = window.document.querySelector(".toggle-theme");
+
+var toogleTheme = true;
+const darkTheme = () => {
+    if(toogleTheme) {
+        window.document.documentElement.style.setProperty("--body", "#0E0E10");
+        window.document.documentElement.style.setProperty("--header", "#CC241D");
+        window.document.documentElement.style.setProperty("--title-color", "#EFEFF1");
+        window.document.documentElement.style.setProperty("--bgcolor", "#18181B");
+        window.document.documentElement.style.setProperty("--text-color", "#FFF");
+        window.document.documentElement.style.setProperty("--btn", "#FE8019");
+    } else {
+        window.document.documentElement.style.setProperty("--body", "#F9F5D8");
+        window.document.documentElement.style.setProperty("--header", "#FB4934");
+        window.document.documentElement.style.setProperty("--title-color", "#3C3836");
+        window.document.documentElement.style.setProperty("--bgcolor", "#FAEFC5");
+        window.document.documentElement.style.setProperty("--text-color", "#282828");
+        window.document.documentElement.style.setProperty("--btn", "#FE8019");
+    }
+    toogleTheme = !toogleTheme;
+}
+
+toggle.addEventListener("click", darkTheme);
